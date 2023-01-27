add988d0e9f38f049b47029aeda1c225efd3fee9
Jacob Fink <jacobifink@gmail.com>
2023-01-27 10:04:29 -0800

    Stars can now be conditionally filled.

--- client/src/components/Stars/Stars.jsx
+++ client/src/components/Stars/Stars.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Star from './Star.jsx'
 
 const Stars = () => {
 
+  let starArr = ['50%', '30%', '40%', '60%', '80%'];
+  let starTotal = 336;
+
+  for (let i = 0; i < 5; i++) {
+
+  }
+
   return (
-    <Star />
+    <div>
+      {starArr.map((starVal, idx) =>
+        <Star starFill={starVal} key={idx} idx={idx} />
+      )}
+    </div>
   );
 }
 
