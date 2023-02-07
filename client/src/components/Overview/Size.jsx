b277d1b43b6b3062248167ad5ae387ddd1ed5607
Pseudo08 <arpan.shrestha57@gmail.com>
2023-02-07 09:29:30 -0800

    Made disabled buttons for out of stock size

--- client/src/components/Overview/Size.jsx
+++ client/src/components/Overview/Size.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 const Size = ({currentInventory, onSizeClick}) => {
+    console.log(currentInventory)
 
     return(<ul className="size-list">
 
-            {Object.keys(currentInventory).length && !Object.keys(currentInventory).includes('null') ? Object.keys(currentInventory).map((size, index) => (
-
-            <li className="size-ind" id= {index} onClick={onSizeClick}>
-            <input type="button" name="size" className="size-btn" value={currentInventory[size].size} required="required"></input>
-            </li>
+            {Object.keys(currentInventory).length && !Object.keys(currentInventory).includes('null')
+            ? Object.keys(currentInventory).map((size, index) => (
+                currentInventory[size].quantity ?
+                <li className="size-ind" id= {index} onClick={onSizeClick}>
+                <input type="button" name="size" className="size-btn" value={currentInventory[size].size} required="required"></input>
+                </li> :
+                <li className="size-ind" id= {index} onClick={onSizeClick}>
+                <input type="button" name="size" className="size-btn" value={currentInventory[size].size} disabled></input>
+                </li>
             ))
         : <li className="size-ind" id= "oneSize" onClick={onSizeClick}>
         <input type="button" id= "onesize" name="size" className="size-btn" value="One Size" required="required"></input>
