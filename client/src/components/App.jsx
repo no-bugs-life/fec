a6f09271e411e9d4972f2cbb0c45c876b7dc26db
Mark <not.markyun@gmail.com>
2023-02-08 12:15:14 -0800

    responsive css start

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -33,12 +33,12 @@ const App = ()=> {
   return(
     <div className="product-main app">
       {/* Overview */}
-      {Object.keys(product).length > 0 ? <Description product ={product}/> : null}
+      {/* {Object.keys(product).length > 0 ? <Description product ={product}/> : null} */}
 
       {/* Review & Ratings */}
-      <ReviewList product_id={40352} productName={product.name}/>
+      {/* <ReviewList product_id={40352} productName={product.name}/>
       <br/>
-      <br/>
+      <br/> */}
       {/* Questions */}
       {/* <div className="questions">
         <QuestionMounted product={product} setProduct={setProduct}/>
