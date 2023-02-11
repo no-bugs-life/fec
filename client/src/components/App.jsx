6d2646a527408b91291bbe53027385455b384f22
Mark <not.markyun@gmail.com>
2023-02-11 11:28:03 -0800

    app fix

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -43,26 +43,28 @@ const App = ()=> {
 
     <div className="product-main app">
       <Suspense fallback= {<div><LoadingScreen /></div>}>
-        <div className="top">
-          <div className='top-skull'>
-            <Skull color="#E8EDDF"/>
-          </div>
-          <span className="logo">Cold Topic</span>
-        </div>
+        {
+          Object.keys(product).length > 0 ?
+            <>
+              <div className="top">
+                <div className='top-skull'>
+                  <Skull color="#E8EDDF"/>
+                </div>
+                <span className="logo">Cold Topic</span>
+              </div>
 
-        {/* Overview */}
-        <Description product ={product}/>
-        {/* Review & Ratings */}
-        <ReviewList product_id={40352} productName={product.name}/>
-        {/* Questions */}
-        <QuestionMounted product={product} setProduct={setProduct}/>
-        {/* Related Items & Comparison */}
-        <RelatedList
-          product={product}
-        />
-        <OutfitList
-          product={product}
-        />
+              <Description product ={product}/>
+
+              <ReviewList product_id={40352} productName={product.name}/>
+
+              <QuestionMounted product={product} setProduct={setProduct}/>
+
+              <RelatedList product={product}/>
+              <OutfitList product={product}/>
+            </>
+          :
+          null
+        }
       </Suspense>
 
     </div>
